7b070dbdc07047497d1448e44deb34be75b77b84
Pierro0705 <33931468+Pierro0705@users.noreply.github.com>
2019-11-26 12:49:03 +0100

    Inscription

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -4,16 +4,69 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use App\Entity\Client;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
 
 class InscriptionController extends AbstractController
 {
     /**
      * @Route("/inscription", name="inscription")
      */
-    public function index()
+    public function create(Request $request, EntityManagerInterface $manager)
     {
+        $client = new Client();
+
+        $form = $this->createFormBuilder($client)
+                     ->add('mail', EmailType::class, [
+                         'attr' => [
+                            'placeholder' => 'Email',
+                            'class' => 'form-control'
+                        ]
+                    ])
+                     ->add('nom' , TextType::class, [
+                        'attr' => [
+                            'placeholder' => 'Nom',
+                            'class' => 'form-control'
+                        ]
+                    ])
+                     ->add('prenom' , TextType::class, [
+                        'attr' => [
+                            'placeholder' => 'Prénom',
+                            'class' => 'form-control'
+                        ]
+                    ])
+                     ->add('mdp' , PasswordType::class,  [
+                        'attr' => [
+                            'placeholder' => 'Mot de passe',
+                            'class' => 'form-control'
+                        ]
+                    ])
+                     ->add('valider' , SubmitType::class,  [
+                        'attr' => [
+                            'placeholder' => 'Valider',
+                            'class' => 'btn south-btn'
+                        ]
+                    ])
+                     ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $manager->persist($client);
+            $manager->flush();
+
+            return $this ->redirectToRoute('accueil');
+        }
+
         return $this->render('inscription/index.html.twig', [
-            'controller_name' => 'InscriptionController',
+            'formClient' => $form->createView()
         ]);
     }
 }
